1ca1d32e684c021f5e2abd7c13c381370c2cd5b8
Yangan <anyang_2006@126.com>
2016-05-27 20:19:54 +0800

    some edit

--- client/Client/src/NewPriceFrame.java
+++ client/Client/src/NewPriceFrame.java
@@ -1,48 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 import java.awt.Font;
 import java.awt.event.WindowEvent;
